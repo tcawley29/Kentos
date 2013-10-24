ed5241234053dbbf538f23c1bd0bbdd812a1cc71
tcawley29 <tommy.cawley@hotmail.com>
2013-10-24 15:53:10 +0100

    Fourth Commit

--- Kentos/Kentos/Register.aspx.cs
+++ Kentos/Kentos/Register.aspx.cs
@@ -23,16 +23,18 @@ namespace Kentos
             con.Open();
             SqlDataSource SQLUser = new SqlDataSource();
             SQLUser.ConnectionString = connectionString;
-            SQLUser.InsertCommand = "INSERT INTO [Users] ([FirstName], [Surname], [Address1], [Address2], [Address3], [County], [Username], [Password], [Email]) VALUES (@FirstName, @Surname, @Address1, @Address2, @Address3, @County, @Username, @Password, @Email)";
+            SQLUser.InsertCommand = "INSERT INTO [KentosUsers] ([FirstName], [Surname], [Address1], [Address2], [Address3], [County], [Username], [Password], [Email]) VALUES (@FirstName, @Surname, @Address1, @Address2, @Address3, @County, @Username, @Password, @Email)";
             SQLUser.InsertParameters.Add("FirstName", txtFname.Text);
             SQLUser.InsertParameters.Add("Surname", txtSname.Text);
             SQLUser.InsertParameters.Add("Address1", txtAdd1.Text);
             SQLUser.InsertParameters.Add("Address2", txtAdd2.Text);
             SQLUser.InsertParameters.Add("Address3", txtAdd3.Text);
             SQLUser.InsertParameters.Add("County", txtCounty.Text);
+            SQLUser.InsertParameters.Add("Email", txtEmail.Text);
+            SQLUser.Insert();
+            SQLUser.InsertCommand = "INSERT INTO [LoginTable] ([username], [password]) VALUES (@Username, @Password)";
             SQLUser.InsertParameters.Add("Username", txtUsername.Text);
             SQLUser.InsertParameters.Add("Password", txtPassword.Text);
-            SQLUser.InsertParameters.Add("Email", txtEmail.Text);
             SQLUser.Insert();
             con.Close();
         }
